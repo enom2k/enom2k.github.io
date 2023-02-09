d7bd191067d2ff4bcd39b151374a249d735e4be4
enom2k <88424886+enom2k@users.noreply.github.com>
2023-02-09 18:04:45 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 const body = document.querySelector('body');
-  let scrollPosition = 0;
+let scrollPosition = 0;
 
-  //body fixed
-  function enable() {
-    scrollPosition = window.pageYOffset;
-    body.style.overflow = 'hidden';
-    body.style.position = 'fixed';
-    body.style.top = `-${scrollPosition}px`;
-    body.style.width = '100%';
-  }
+//body fixed
+function enable() {
+  scrollPosition = window.pageYOffset;
+  body.style.overflow = 'hidden';
+  body.style.position = 'fixed';
+  body.style.top = `-${scrollPosition}px`;
+  body.style.width = '100%';
+}
 
-  function disable() {
-    body.style.removeProperty('overflow');
-    body.style.removeProperty('position');
-    body.style.removeProperty('top');
-    body.style.removeProperty('width');
-    window.scrollTo(0, scrollPosition);
-  }
+function disable() {
+  body.style.removeProperty('overflow');
+  body.style.removeProperty('position');
+  body.style.removeProperty('top');
+  body.style.removeProperty('width');
+  window.scrollTo(0, scrollPosition);
+}
 
 const load = gsap.timeline({
   paused: true,
